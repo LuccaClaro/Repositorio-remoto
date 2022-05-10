ca70a8f57dd098cc53d3e4bd028f61493e8797d9
Lucca Claro <luccaoc@al.insper.edu.br>
2022-05-10 11:57:00 -0300

    Funções

--- Funções.py
+++ Funções.py
@@ -6,3 +6,17 @@ def normaliza(dicio):
             banco_de_dados[c2]["continente"]=c
 
     return banco_de_dados
+
+import random
+def sorteia_pais(dicio):
+    lista_paises = []
+    for i in dicio.keys():
+        lista_paises.append(i)
+    return (random.choice(lista_paises))
+
+def esta_na_lista(pais, lista):
+    for a in lista:
+        for i in a:
+            if pais in str(i):
+                return True
+    return False
